261a3d66c555f69ffd9519442862292cfde7c5cb
Om Lachake <omlachake@protonmail.com>
2022-11-08 20:54:28 +0530

    Update - Stripe Fix 2

--- next.config.js
+++ next.config.js
@@ -4,15 +4,32 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  webpack: (config, options) => {
-    config.module.rules.push({
-      target: "node",
-      dllplugins: {
-        defaults: {
-          exclude: ["stripe"],
-        },
-      },
-    });
+  webpack: (config, { isServer, webpack }) => {
+    if (!isServer) {
+      config.node = {
+        dgram: "empty",
+        fs: "empty",
+        net: "empty",
+        tls: "empty",
+        child_process: "empty",
+      };
+
+      config.plugins.push(new webpack.IgnorePlugin(/^(stripe)$/));
+      config.plugins.push(
+        new webpack.IgnorePlugin({
+          checkResource(resource, context) {
+            // If I am including something from my backend directory, I am sure that this shouldn't be included in my frontend bundle
+            if (
+              resource.includes("/backend/") &&
+              !context.includes("node_modules")
+            ) {
+              return true;
+            }
+            return false;
+          },
+        })
+      );
+    }
 
     return config;
   },
