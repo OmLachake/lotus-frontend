5bbfbddeb3d64401fb7ace3ca18d1f65261ca6fa
Om Lachake <omlachake@protonmail.com>
2022-11-13 19:34:51 +0530

    Update - Stripe error Fixed, order.[id] page fixed

---
+++ pages/profile/orders/[id].js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import React from "react";
+
+const OrderID = () => {
+  return (
+    <div>
+      <Head>
+        <title>Order</title>
+        <meta name="description" content="" />
+      </Head>
+      <main>
+        <div
+          className="flex justify-center items-center
+bg-white min-h-[50vh] rounded-md border text-pink font-josan font-light text-4xl"
+        >
+          Coming Soon...
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default OrderID;
